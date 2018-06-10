9d4538ed27e6f755354193da473c376b91ed6aaa
Erik Soderstrom <git@soderstrome.com>
2018-06-10 01:11:22 -0400

    Removed space from css file path.

--- account/index.php
+++ account/index.php
@@ -10,7 +10,7 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
 
-	<link rel="stylesheet" href="/css /beststyle2018.css"> 
+	<link rel="stylesheet" href="/css/beststyle2018.css"> 
     <title>Your UpDog Account Details</title>
 	
 	<link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
